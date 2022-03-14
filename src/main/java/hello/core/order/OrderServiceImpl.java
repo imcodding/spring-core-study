18ca75a021c99a01d5fc6dfc5e439b9fed4e5e6d
imcodding <suy0226@naver.com>
2022-03-15 00:56:07 +0900

    [part2] 객체지향 원리(1) - DIP, OCP 위반

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,12 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //만약 할인율로 변경된다면? 아래처럼 변경
+    //DIP 위반: 추상(인터페이스)에 의존해야 하는데 구현 클래스에도 의존하고 있음
+    //OCP 위반: 확장해서 변경할 경우, 클라이언트 코드에 영향줌
+    //해결방안은? 인터페이스만 의존하도록 해야함. 누군가가 구현 객체를 대신 생성하여 주입해줘야함
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
