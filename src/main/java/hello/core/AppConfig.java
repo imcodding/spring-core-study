cae2b86763b534c2ffa4fe7770d5367aa05701da
imcodding <suy0226@naver.com>
2022-03-15 22:47:19 +0900

    [part2] AppConfig 리팩토링
    1) 역할이 뚜렷하게 보임
    2) 중복 제거됨. 변경 용이

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,12 +11,21 @@ import hello.core.order.OrderServiceImpl;
 // 애플리케이션 전체 설정
 // 실제 동작을 위한 구현 객체를 생성함
 public class AppConfig {
+    // 리택토링 - 역할을 명확하게 보이도록 변경. 중복 제거
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
