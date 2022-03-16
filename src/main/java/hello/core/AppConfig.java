e2bb5af7f65c0a1a16c0f057e12ed7180f525930
imcodding <suy0226@naver.com>
2022-03-16 23:26:40 +0900

    [part3] 스프링 빈(1)
    빈 생성 및 조회

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,24 +8,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 애플리케이션 전체 설정
 // 실제 동작을 위한 구현 객체를 생성함
+@Configuration // 스프링 컨터이너가 구성정보로 사용함. @Bean 객체를 모두 생성해서 스프링 컨테이너에 등록
 public class AppConfig {
     // 리택토링 - 역할을 명확하게 보이도록 변경. 중복 제거
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();//정액
         return new RateDiscountPolicy();//정률
