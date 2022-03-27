84512ec5617be7509fdc894d2f77d47d4381554d
imcodding <suy0226@naver.com>
2022-03-27 21:25:39 +0900

    [part6] lombok 사용
    - 최근에는 생상자를 1개만 두고 @Autowired 생략하거나 lombok 사용함

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,25 +3,15 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final 이 붙은 필드를 찾아서 생성자를 자동으로 만들어준다.
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    /* 생성자 주입 방식을 사용해라!
-    1. 대부분의 의존 관계는 애플리케이션 종료 전까지 변경되면 안 된다.
-    2. setter 주입 방식은 public 로 열어둬야해서 취약하고, 누군가 실수로 변경할 수 있다.
-    3. 프레임워크에 의존하지 않고, 순수한 자바 언어의 특징을 살릴 수 있다.
-    **/
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
