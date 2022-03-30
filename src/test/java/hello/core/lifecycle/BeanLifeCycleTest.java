e87e19a895a3c0b7764d60e0c9b96b1172c7d856
imcodding <suy0226@naver.com>
2022-03-30 18:21:59 +0900

    [part7] 빈 생명주기 콜백(2) - @PostConstruct, @PreDestory
    사용 권장. 단, 외부 라이브러리에 적용 못 함.

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -19,13 +19,7 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifecycleConfig {
 
-        /* 특징
-        1. 초기화, 소멸 메소드 이름을 자유롭게 설정 가능
-        2. 스프링에 의존하지 않음.
-        3. 외부 라이브러리에 적용 가능.
-        단, destroyMethod 는 default 로 close 또는 shutdown 메서드 있으면 호출
-        * */
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,9 @@
 package hello.core.lifecycle;
 
+// 스프링에 의존적이지 않음. 자바 표준
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -26,12 +30,14 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
